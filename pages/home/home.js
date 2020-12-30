487a8d4f1f979b3d8891af31fac2bf2a82442211
menfutong <menfutong2020@163.com>
2020-12-30 08:57:31 +0800

    04_page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,36 +1,66 @@
 Page({
-  data:{
-    name:'menfutong',
-    age:18,
-    students:[
-      {id:110,name:'kobe',age:30},
-      {id:111,name:'james',age:30},
-      {id:112,name:'curry',age:30},
-      {id:113,name:'why',age:18},
-    ],
-    counter:0
+  //----------2.初始化数据------------
+  data: {
+    message: "哈哈哈",
+    list: [],
+    url: ""
   },
-  handleBtnClick() {
-    console.log('按钮发生了点击');
-    //1.错误做法:界面是不会刷新的
-    this.data.counter++;
-    console.log(this.data.counter);
+  // --------------1. 监听页面的生命周期函数-----------------------
+  // 页面加载进来
+  onLoad() {
+    console.log('OnLoad');
 
-
-    //2. this.setData
-    this.setData({
-      counter:this.data.counter++
+    const _this = this;
+    wx.request({
+        url: 'http://www.imedbooks.com/web/book/getMoreBooks?rows=5&page=1&type=1&searchType=2&classId=&top=0',
+        // 箭头函数中的this一层层向上找
+        // success: (res) => {
+        //   console.log(res);
+        //   const data = res.data.data;
+        //   console.log(data)
+        //   //this.data.list = data;
+        //   this.setData({
+        //     list: data
+        //   })
+        // }
+      
+        //不是箭头函数就要在上层提前声明this
+        success: function (res) {
+          console.log(this)
+          const data = res.data.data;
+          _this.setData({
+            list: data
+          })
+        }
     })
-  },
-  handleSubtraction() {
-    console.log('定义了减号');
-    this.setData({
-      counter:this.data.counter-1
-    }) 
-  }
+},
 
+//----------------3. 监听wxml中相关的一些事件--------------------------------
+handleGetUserInfo(event) {
+  wx.getUserInfo({
+    success: function (res) {
+      console.log(res.userInfo.avatarUrl);
+      //this.data.url=res.userInfo.avatarUrl;
+    }
+  })
+},
+handleViewClick() {
+  console.log('哈哈哈!!!我中奖了!!!!')
+},
 
-  //编程范式:
-  //1.命令式编程 原生操作DOM 
-  //2.声明式编程 Vue/React/Angular
+//------------4.监听其他事件-------------
+//监听页面的滚动
+onPageScroll(obj) {
+  //console.log(obj)
+},
+
+//监听页面滚动到底部
+onReachBottom() {
+  console.log('页面滚动到底部')
+},
+
+onPullDownRefresh() {
+  console.log('下拉刷新')
+}
+
 })
