c0f5fd76906af5dc1db2806e0b68c993c777ab7c
menfutong <menfutong2020@163.com>
2021-03-10 15:50:32 +0800

    11_弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,75 @@
 // pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
+Page({
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function () {
+        console.log('success');
+      },
+      fail: function () {
+        console.log('fail');
+      },
+      complete: function () {
+        console.log('complete')
+      }
+    })
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '战狼3',
+      // showCancel:false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function (res) {
+        console.log(res);
+        if (res.cancel) {
+          console.log('用户点击取消')
+        }
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      },
+    })
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
+
+    setTimeout(() => {
+      //必须手动调用hideLoading才会让loading消失 
+      wx.hideLoading()
+    }, 1000)
+  },
+
+  handleActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function (res) {
+        console.log(res)
+        
+      }
+    })
+  },
+
+  onShareAppMessage:function(option) {
+    return {
+      title:'你妹的',
+      path:'/pages/about/about',
+      imageUrl:'https://img12.360buyimg.com/pop/s1180x940_jfs/t1/97205/26/1142/87801/5dbac55aEf795d962/48a4d7a63ff80b8b.jpg'
+    }
   }
 })
+
+
+
+
